6e56b32e972c9c145e3d8b287fa0f7b83240f145
Harmanpreetkaur <42209128+harmankaurjass@users.noreply.github.com>
2022-04-03 16:46:45 -0400

    Create keys.js

---
+++ client/src/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    CLIENT_ID:'100258798541-61g20dks0n1qtcsnle758e9v971e69ok.apps.googleusercontent.com',
+    STRIPE_PS_KEY: 'pk_test_51KkGwSA9lDw50m9uTfbPc3mlb3Xa878XvtHORRxliba03eqYGGI9LdKBExduQVFjEfAydg4WtoeqhcXPkgmKcGR1006dERKeXz'
+}
