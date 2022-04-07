e9866f3e3b80ca97a9cb1897b95be4cae92af251
unknown <ksingh3300@conestogac.on.ca>
2022-04-06 20:08:48 -0400

    Adding build and node modules

---
+++ client/build/precache-manifest.1fc802bdbc3f70472b539f2fedb66d57.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877fca9c40c5b9f00435f66f48ed00de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c7cb064913c2f4e3a77",
+    "url": "/static/css/main.e1fea530.chunk.css"
+  },
+  {
+    "revision": "ef8f3b3042d26b6d0071",
+    "url": "/static/js/2.e38a9425.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.e38a9425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7cb064913c2f4e3a77",
+    "url": "/static/js/main.3e55ace2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "7f36c7f99fe2f473efed22b5f91bcb9e",
+    "url": "/static/media/1.7f36c7f9.jpg"
+  },
+  {
+    "revision": "fa2fbdcd8ab18c468eca17887593107f",
+    "url": "/static/media/1.fa2fbdcd.jpg"
+  },
+  {
+    "revision": "9435fa042a3290f6a9ff5cc85fae9b93",
+    "url": "/static/media/10.9435fa04.jpg"
+  },
+  {
+    "revision": "14c28c951b9f2b410a8c5936adfa600b",
+    "url": "/static/media/11.14c28c95.jpg"
+  },
+  {
+    "revision": "ad6befd28c2640bd9125c5b943153a66",
+    "url": "/static/media/11.ad6befd2.jpg"
+  },
+  {
+    "revision": "a0590ecc98a4e736466732511bd3a035",
+    "url": "/static/media/12.a0590ecc.jpg"
+  },
+  {
+    "revision": "b3d9b59237505eb7103e1edd4a4921e2",
+    "url": "/static/media/13.b3d9b592.jpg"
+  },
+  {
+    "revision": "367e04db9448b7cb8691dd35aa77d5c8",
+    "url": "/static/media/14.367e04db.jpg"
+  },
+  {
+    "revision": "279fd76379b7010c4c9aacde7a51a107",
+    "url": "/static/media/15.279fd763.jpg"
+  },
+  {
+    "revision": "0ebcb8da6f5cacca021228e8bebab069",
+    "url": "/static/media/16.0ebcb8da.jpg"
+  },
+  {
+    "revision": "a602ded726754b57d14f85bcbd9dcec5",
+    "url": "/static/media/17.a602ded7.jpg"
+  },
+  {
+    "revision": "d45a8ae8455f941cc31f1ad56c6d3e1c",
+    "url": "/static/media/18.d45a8ae8.jpg"
+  },
+  {
+    "revision": "db6bbb16644380c071dc721ea6baea61",
+    "url": "/static/media/19.db6bbb16.jpg"
+  },
+  {
+    "revision": "8cb8dad0ae6245b375b56781451d7892",
+    "url": "/static/media/2.8cb8dad0.jpg"
+  },
+  {
+    "revision": "b194b5d46fff8cd7e59e343fdbb6e8de",
+    "url": "/static/media/2.b194b5d4.jpg"
+  },
+  {
+    "revision": "3a45c4389cd477d58ceb4524959c8026",
+    "url": "/static/media/20.3a45c438.jpg"
+  },
+  {
+    "revision": "cabe365758ce61265fe1706858c9258e",
+    "url": "/static/media/21.cabe3657.jpg"
+  },
+  {
+    "revision": "088fed9be5790aaaee7ad87709490728",
+    "url": "/static/media/22.088fed9b.jpg"
+  },
+  {
+    "revision": "ad2340ea082c34127dd37e6af35f8dd0",
+    "url": "/static/media/22.ad2340ea.jpg"
+  },
+  {
+    "revision": "35c58882578bfbdaa1ebc4c9b93f0cf0",
+    "url": "/static/media/23.35c58882.jpg"
+  },
+  {
+    "revision": "4a72861eacc8fe92187a988ef511e9ee",
+    "url": "/static/media/24.4a72861e.jpg"
+  },
+  {
+    "revision": "4fc295a9288a7c945a057d2b1c14fa90",
+    "url": "/static/media/3.4fc295a9.jpg"
+  },
+  {
+    "revision": "d88cde0a4f01840e6ab15dbf5f767805",
+    "url": "/static/media/3.d88cde0a.jpg"
+  },
+  {
+    "revision": "4f6fa94e246754d91b35f95c1d65de16",
+    "url": "/static/media/33.4f6fa94e.png"
+  },
+  {
+    "revision": "8a3f6652541ca65366d3384e98d0b4f2",
+    "url": "/static/media/4.8a3f6652.jpg"
+  },
+  {
+    "revision": "8d6594bcabde445364752fedc6330d1e",
+    "url": "/static/media/6.8d6594bc.jpg"
+  },
+  {
+    "revision": "75d838e2bf5cd7f48ec7555a9366e68d",
+    "url": "/static/media/7.75d838e2.jpg"
+  },
+  {
+    "revision": "2ee94de400069c30953e839be2a81a25",
+    "url": "/static/media/8.2ee94de4.jpg"
+  },
+  {
+    "revision": "d4a41da58295cd925ddc093927aa4d7a",
+    "url": "/static/media/9.d4a41da5.jpg"
+  },
+  {
+    "revision": "3da50a7f28264009b4df8ca589a7766b",
+    "url": "/static/media/about1.3da50a7f.jpg"
+  },
+  {
+    "revision": "1eed58ee189cf9743b2d22e759d0025e",
+    "url": "/static/media/books_success.1eed58ee.jpg"
+  },
+  {
+    "revision": "4ead8a32d8f8850099a58809df5be96f",
+    "url": "/static/media/error404.4ead8a32.jpg"
+  },
+  {
+    "revision": "89610a593939be4c31590bdcdb06f67d",
+    "url": "/static/media/no_cover.89610a59.jpg"
+  },
+  {
+    "revision": "85f01a724ee37a66996f13594c9f0283",
+    "url": "/static/media/spinner.85f01a72.gif"
+  }
+]);
